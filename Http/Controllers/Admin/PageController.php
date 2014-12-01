bcaccb00494e37b4119aa70068b26eaf289fae98
Nicolas Widart <n.widart@gmail.com>
2014-12-01 21:45:18 +0100

    Adding index view

--- Http/Controllers/Admin/PageController.php
+++ Http/Controllers/Admin/PageController.php
@@ -2,13 +2,27 @@
 
 use Illuminate\Support\Facades\View;
 use Modules\Core\Http\Controllers\Admin\AdminBaseController;
+use Modules\Page\Repositories\PageRepository;
 
 class PageController extends AdminBaseController
 {
 
+    /**
+     * @var PageRepository
+     */
+    private $page;
+
+    public function __construct(PageRepository $page)
+    {
+        parent::__construct();
+        $this->page = $page;
+    }
+
     public function index()
     {
-        return View::make('page::admin.index');
+        $pages = $this->page->all();
+
+        return View::make('page::admin.index', compact('pages'));
     }
 
 }
--- Resources/views/admin/index.blade.php
+++ Resources/views/admin/index.blade.php
@@ -12,8 +12,120 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
-            <p>{{ trans('dashboard::dashboard.welcome-message') }}</p>
+        <div class="col-xs-12">
+            <div class="row">
+                <div class="btn-group pull-right" style="margin: 0 15px 15px 0;">
+                    <a href="{{ URL::route('dashboard.page.create') }}" class="btn btn-primary btn-flat" style="padding: 4px 10px;">
+                        <i class="fa fa-pencil"></i> {{ trans('page::page.button.create page') }}
+                    </a>
+                </div>
+            </div>
+            <div class="box box-info">
+                <div class="box-header">
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body table-responsive">
+                    <table class="data-table table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th>{{ trans('core::core.table.created at') }}</th>
+                            <th>{{ trans('page::page.table.name') }}</th>
+                            <th>{{ trans('page::page.table.slug') }}</th>
+                            <th>{{ trans('core::core.table.actions') }}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php if (isset($pages)): ?>
+                        <?php foreach($pages as $page): ?>
+                        <tr>
+                            <td>
+                                <a href="{{ URL::route('dashboard.page.edit', [$page->id]) }}">
+                                    {{ $page->created_at }}
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{ URL::route('dashboard.page.edit', [$page->id]) }}">
+                                    {{ $page->name }}
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{ URL::route('dashboard.page.edit', [$page->id]) }}">
+                                    {{ $page->slug }}
+                                </a>
+                            </td>
+                            <td>
+                                <div class="btn-group">
+                                    <a href="{{ URL::route('dashboard.page.edit', [$page->id]) }}" class="btn btn-default btn-flat"><i class="glyphicon glyphicon-pencil"></i></a>
+                                    <button class="btn btn-danger btn-flat" data-toggle="modal" data-target="#confirmation-{{ $page->id }}"><i class="glyphicon glyphicon-trash"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                        <?php endif; ?>
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <th>{{ trans('core::core.table.created at') }}</th>
+                            <th>{{ trans('page::page.table.name') }}</th>
+                            <th>{{ trans('page::page.table.slug') }}</th>
+                            <th>{{ trans('core::core.table.actions') }}</th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
         </div>
     </div>
+    <?php if (isset($pages)): ?>
+    <?php foreach($pages as $page): ?>
+    <!-- Modal -->
+    <div class="modal fade" id="confirmation-{{ $page->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="myModalLabel">{{ trans('core::core.modal.title') }}</h4>
+                </div>
+                <div class="modal-body">
+                    {{ trans('core::core.modal.confirmation-message') }}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ trans('core::core.button.cancel') }}</button>
+                    {!! Form::open(['route' => ['dashboard.page.destroy', $page->id], 'method' => 'delete', 'class' => 'pull-left']) !!}
+                    <button type="submit" class="btn btn-danger btn-flat"><i class="glyphicon glyphicon-trash"></i> {{ trans('core::core.button.delete') }}</button>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php endforeach; ?>
+    <?php endif; ?>
+@stop
+
+@section('scripts')
+    <?php $locale = App::getLocale(); ?>
+    <script type="text/javascript">
+        $(function () {
+            $('.data-table').dataTable({
+                "paginate": true,
+                "lengthChange": true,
+                "filter": true,
+                "sort": true,
+                "info": true,
+                "autoWidth": true,
+                "order": [[ 0, "desc" ]],
+                "language": {
+                    "url": '<?php echo Module::asset("core:js/vendor/datatables/{$locale}.json") ?>'
+                },
+                "columns": [
+                    null,
+                    null,
+                    null,
+                    { "sortable": false }
+                ]
+            });
+        });
+    </script>
 @stop
