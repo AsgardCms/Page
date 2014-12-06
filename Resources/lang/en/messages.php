273f266d188ad52eba2cf449fc7a6467e55d30ae
Nicolas Widart <n.widart@gmail.com>
2014-12-06 10:44:57 +0100

    Adding more translation messages

--- Resources/lang/en/messages.php
+++ Resources/lang/en/messages.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
-    'page created' => 'Page created.',
+    'page created' => 'Page successfully created.',
+    'page not found' => 'Page not found.',
+    'page updated' => 'Page successfully updated.',
+    'page deleted' => 'Page successfully deleted.',
 
     'template is required' => 'The template name is required.',
 ];
--- Resources/lang/fr/messages.php
+++ Resources/lang/fr/messages.php
@@ -2,6 +2,9 @@
 
 return [
     'page created' => 'Page créer.',
+    'page not found' => 'Page non trouvé.',
+    'page updated' => 'Page mit à jour.',
+    'page deleted' => 'Page suprimer',
 
     'template is required' => 'Le nom de template de la page est réquis.',
 ];
