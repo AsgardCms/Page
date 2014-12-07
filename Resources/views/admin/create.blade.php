22052f1ecb2f6d77b62fd807461ee29eb9b0294e
Nicolas Widart <n.widart@gmail.com>
2014-12-07 10:43:13 +0100

    Make the cancel links redirect to page index

--- Resources/views/admin/create.blade.php
+++ Resources/views/admin/create.blade.php
@@ -33,7 +33,7 @@
 
                     <div class="box-footer">
                         <button type="submit" class="btn btn-primary btn-flat">{{ trans('core::core.button.create') }}</button>
-                        <a class="btn btn-danger pull-right btn-flat" href="{{ URL::route('dashboard.category.index')}}"><i class="fa fa-times"></i> {{ trans('core::core.button.cancel') }}</a>
+                        <a class="btn btn-danger pull-right btn-flat" href="{{ URL::route('dashboard.page.index')}}"><i class="fa fa-times"></i> {{ trans('core::core.button.cancel') }}</a>
                     </div>
                 </div>
             </div> {{-- end nav-tabs-custom --}}
