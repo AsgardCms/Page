09162fba9dab7eca89624d68fbf53f15ea6f7aa0
Nicolas Widart <n.widart@gmail.com>
2014-12-09 20:25:13 +0100

    Add some better content for homepage

--- Database/Seeders/BasePageDatabaseSeeder.php
+++ Database/Seeders/BasePageDatabaseSeeder.php
@@ -24,7 +24,11 @@ class BasePageDatabaseSeeder extends Seeder
             'en' => [
                 'title' => 'Home page',
                 'slug' => 'home',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum iure necessitatibus repellat sapiente sit. Alias dolore id, nam odit reiciendis similique vel voluptatem! Alias assumenda beatae doloremque iste magni provident quae reiciendis saepe tempora voluptatibus! Animi, asperiores consectetur cum delectus explicabo fuga neque, quam quidem reiciendis similique unde voluptates! Quibusdam.',
+                'body' => '<p><strong>You made it!</strong></p>
+<p>You&#39;ve installed AsgardCMS and are ready to proceed to the <a href="http://asgard-test1/en/backend">administration area</a>.</p>
+<h2>What&#39;s next ?</h2>
+<p>Learn how you can develop modules for AsgardCMS by reading our <a href="https://github.com/AsgardCms/Documentation">documentation</a>.</p>
+',
                 'meta_title' => 'Home page'
             ],
             'fr' => [
