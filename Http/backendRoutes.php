2b2474610e0128a0e5f6e0d126b7008a6732289a
Nicolas Widart <n.widart@gmail.com>
2015-02-12 10:34:55 +0100

    Using the new routeSP. Separating the route types.

---
+++ Http/backendRoutes.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Routing\Router;
+/** @var Router $router */
+
+$router->model('pages', 'Modules\Page\Entities\Page');
+
+$router->group(['prefix' => '/page'], function (Router $router) {
+    $router->resource('pages', 'PageController', ['except' => ['show'], 'names' => [
+        'index' => 'admin.page.page.index',
+        'create' => 'admin.page.page.create',
+        'store' => 'admin.page.page.store',
+        'edit' => 'admin.page.page.edit',
+        'update' => 'admin.page.page.update',
+        'destroy' => 'admin.page.page.destroy',
+    ]]);
+});
---
+++ Http/frontendRoutes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Routing\Router;
+/** @var Router $router */
+
+if (! App::runningInConsole()) {
+    $prefix = config('asgard.core.core.admin-prefix');
+    $router->get('{uri}', 'PublicController@uri')->where('uri', "(?!$prefix).*");
+    $router->get('/', 'PublicController@homepage');
+}
