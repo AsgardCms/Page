24ea6f3116261033b2687a6e50afa2f6be4c24f2
Nicolas Widart <n.widart@gmail.com>
2015-02-17 08:47:32 +0100

    Make the backend link relative

--- Database/Seeders/PageDatabaseSeeder.php
+++ Database/Seeders/PageDatabaseSeeder.php
@@ -27,7 +27,7 @@ class PageDatabaseSeeder extends Seeder
                 'title' => 'Home page',
                 'slug' => 'home',
                 'body' => '<p><strong>You made it!</strong></p>
-<p>You&#39;ve installed AsgardCMS and are ready to proceed to the <a href="http://asgard-test1/en/backend">administration area</a>.</p>
+<p>You&#39;ve installed AsgardCMS and are ready to proceed to the <a href="/en/backend">administration area</a>.</p>
 <h2>What&#39;s next ?</h2>
 <p>Learn how you can develop modules for AsgardCMS by reading our <a href="https://github.com/AsgardCms/Documentation">documentation</a>.</p>
 ',
