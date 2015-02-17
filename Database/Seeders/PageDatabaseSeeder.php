63a40037353853ed8f73787347662ae0974d057d
Nicolas Widart <n.widart@gmail.com>
2015-02-17 08:40:02 +0100

    Remove the french home page data. Only seed an English page.

--- Database/Seeders/PageDatabaseSeeder.php
+++ Database/Seeders/PageDatabaseSeeder.php
@@ -33,12 +33,6 @@ class PageDatabaseSeeder extends Seeder
 ',
                 'meta_title' => 'Home page',
             ],
-            'fr' => [
-                'title' => 'Page d\'accueil',
-                'slug' => 'accueil',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum iure necessitatibus repellat sapiente sit. Alias dolore id, nam odit reiciendis similique vel voluptatem! Alias assumenda beatae doloremque iste magni provident quae reiciendis saepe tempora voluptatibus! Animi, asperiores consectetur cum delectus explicabo fuga neque, quam quidem reiciendis similique unde voluptates! Quibusdam.',
-                'meta_title' => 'Page d\'accueil',
-            ],
         ];
         $this->page->create($data);
     }
