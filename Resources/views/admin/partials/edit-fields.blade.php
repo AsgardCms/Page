f2f12fa592accda61d775b7dd3f59d44fbfb114d
Nicolas Widart <n.widart@gmail.com>
2015-04-09 19:12:29 +0200

    Add more has translations check

--- Resources/views/admin/partials/edit-fields.blade.php
+++ Resources/views/admin/partials/edit-fields.blade.php
@@ -35,12 +35,14 @@
                 <div class="box-body">
                     <div class='form-group{{ $errors->has("{$lang}[meta_title]") ? ' has-error' : '' }}'>
                         {!! Form::label("{$lang}[meta_title]", trans('page::pages.form.meta_title')) !!}
-                        {!! Form::text("{$lang}[meta_title]", Input::old("{$lang}[meta_title]", $page->translate($lang)->meta_title), ['class' => "form-control", 'placeholder' => trans('page::pages.form.meta_title')]) !!}
+                        <?php $old = $page->hasTranslation($lang) ? $page->translate($lang)->meta_title : '' ?>
+                        {!! Form::text("{$lang}[meta_title]", Input::old("{$lang}[meta_title]", $old), ['class' => "form-control", 'placeholder' => trans('page::pages.form.meta_title')]) !!}
                         {!! $errors->first("{$lang}[meta_title]", '<span class="help-block">:message</span>') !!}
                     </div>
                     <div class='form-group{{ $errors->has("{$lang}[meta_description]") ? ' has-error' : '' }}'>
                         {!! Form::label("{$lang}[meta_description]", trans('page::pages.form.meta_description')) !!}
-                        <textarea class="form-control" name="{{$lang}}[meta_description]" rows="10" cols="80">{{ $page->translate($lang)->meta_description }}</textarea>
+                        <?php $old = $page->hasTranslation($lang) ? $page->translate($lang)->meta_description : '' ?>
+                        <textarea class="form-control" name="{{$lang}}[meta_description]" rows="10" cols="80">{{ $old }}</textarea>
                         {!! $errors->first("{$lang}[meta_description]", '<span class="help-block">:message</span>') !!}
                     </div>
                 </div>
@@ -58,24 +60,27 @@
                 <div class="box-body">
                     <div class='form-group{{ $errors->has("{$lang}[og_title]") ? ' has-error' : '' }}'>
                         {!! Form::label("{$lang}[og_title]", trans('page::pages.form.og_title')) !!}
-                        {!! Form::text("{$lang}[og_title]", Input::old("{$lang}[og_title]", $page->translate($lang)->og_title), ['class' => "form-control", 'placeholder' => trans('page::pages.form.og_title')]) !!}
+                        <?php $old = $page->hasTranslation($lang) ? $page->translate($lang)->og_title : '' ?>
+                        {!! Form::text("{$lang}[og_title]", Input::old("{$lang}[og_title]", $old), ['class' => "form-control", 'placeholder' => trans('page::pages.form.og_title')]) !!}
                         {!! $errors->first("{$lang}[og_title]", '<span class="help-block">:message</span>') !!}
                     </div>
                     <div class='form-group{{ $errors->has("{$lang}[og_description]") ? ' has-error' : '' }}'>
                         {!! Form::label("{$lang}[og_description]", trans('page::pages.form.og_description')) !!}
-                        <textarea class="form-control" name="{{$lang}}[og_description]" rows="10" cols="80">{{ $page->translate($lang)->og_description }}</textarea>
+                        <?php $old = $page->hasTranslation($lang) ? $page->translate($lang)->og_description : '' ?>
+                        <textarea class="form-control" name="{{$lang}}[og_description]" rows="10" cols="80">{{ $old }}</textarea>
                         {!! $errors->first("{$lang}[og_description]", '<span class="help-block">:message</span>') !!}
                     </div>
                     <div class="form-group{{ $errors->has("{$lang}[og_type]") ? ' has-error' : '' }}">
                         <label>{{ trans('page::pages.form.og_type') }}</label>
+                        <?php $oldType = $page->hasTranslation($lang) ? $page->translate($lang)->og_type : '' ?>
                         <select class="form-control">
-                            <option value="website" {{ $page->translate($lang)->og_type == 'website' ? 'selected' : ''}}>
+                            <option value="website" {{ $oldType == 'website' ? 'selected' : ''}}>
                                 {{ trans('page::pages.facebook-types.website') }}
                             </option>
-                            <option value="product" {{ $page->translate($lang)->og_type == 'product' ? 'selected' : ''}}>
+                            <option value="product" {{ $oldType == 'product' ? 'selected' : ''}}>
                                 {{ trans('page::pages.facebook-types.product') }}
                             </option>
-                            <option value="article" {{ $page->translate($lang)->og_type == 'product' ? 'selected' : ''}}>
+                            <option value="article" {{ $oldType == 'product' ? 'selected' : ''}}>
                                 {{ trans('page::pages.facebook-types.article') }}
                             </option>
                         </select>
