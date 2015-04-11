04cdee43cbbcfa7cddaf517e407807dee19f0666
Nicolas Widart <n.widart@gmail.com>
2015-04-11 22:34:36 +0200

    Remove the french default page

--- Database/Seeders/PageDatabaseSeeder.php
+++ Database/Seeders/PageDatabaseSeeder.php
@@ -33,16 +33,6 @@ class PageDatabaseSeeder extends Seeder
 ',
                 'meta_title' => 'Home page',
             ],
-            'fr' => [
-                'title' => 'Page d\'accueil',
-                'slug' => 'accueil',
-                'body' => '<p><strong>You made it!</strong></p>
-<p>You&#39;ve installed AsgardCMS and are ready to proceed to the <a href="/en/backend">administration area</a>.</p>
-<h2>What&#39;s next ?</h2>
-<p>Learn how you can develop modules for AsgardCMS by reading our <a href="https://github.com/AsgardCms/Documentation">documentation</a>.</p>
-',
-                'meta_title' => 'Page d\'accueil',
-            ],
         ];
         $this->page->create($data);
     }
