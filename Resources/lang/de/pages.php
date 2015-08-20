de8022043c70fdef016bafdac7dce89cc81a2b8c
Nuno Ribeiro <nmpribeiro@gmail.com>
2015-08-20 15:23:59 +0000

    Added de locale and pt

---
+++ Resources/lang/de/pages.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'title' => [
+        'pages' => 'Seiten',
+        'create page' => 'Seite erstellen',
+        'edit page' => 'Bearbeiten einer Seite',
+    ],
+    'button' => [
+        'create page' => 'Seite erstellen',
+    ],
+    'table' => [
+        'name' => 'Namen',
+        'slug' => 'Slug',
+    ],
+    'form' => [
+        'title' => 'Titel',
+        'slug' => 'Slug',
+        'meta_title' => 'Meta-Titel',
+        'meta_description' => 'Meta Beschreibung',
+        'og_title' => 'Facebook Titel',
+        'og_description' => 'Facebook Beschreibung',
+        'og_type' => 'Facebook typ',
+        'template' => 'Page Vorlagnamen',
+        'is homepage' => 'Startseite ?',
+        'body' => 'Körper',
+    ],
+    'validation' => [
+        'attributes' => [
+            'title' => 'titel',
+            'body' => 'körper',
+        ],
+    ],
+    'facebook-types' => [
+        'website' => 'Website',
+        'product' => 'Produkt',
+        'article' => 'Artikle',
+    ],
+    'navigation' => [
+        'back to index' => 'Zurück zum Seiten Index'
+    ]
+];
