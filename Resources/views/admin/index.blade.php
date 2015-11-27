a3842723b9314cee92b072808519fee8e2b7383e
Nicolas Widart <n.widart@gmail.com>
2015-11-27 16:01:56 +0100

    Using the delete modal partial

--- Resources/views/admin/index.blade.php
+++ Resources/views/admin/index.blade.php
@@ -85,26 +85,7 @@
             </div>
         </div>
     </div>
-    <!-- Delete modal -->
-    <div class="modal fade modal-danger" id="modal-delete-confirmation" tabindex="-1" role="dialog" aria-labelledby="delete-confirmation-title" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                    <h4 class="modal-title" id="delete-confirmation-title">{{ trans('core::core.modal.title') }}</h4>
-                </div>
-                <div class="modal-body">
-                    {{ trans('core::core.modal.confirmation-message') }}
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline btn-flat" data-dismiss="modal">{{ trans('core::core.button.cancel') }}</button>
-                    {!! Form::open(['method' => 'delete', 'class' => 'pull-left']) !!}
-                    <button type="submit" class="btn btn-outline btn-flat"><i class="glyphicon glyphicon-trash"></i> {{ trans('core::core.button.delete') }}</button>
-                    {!! Form::close() !!}
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('core::partials.delete-modal')
 @stop
 
 @section('footer')
@@ -126,12 +107,6 @@
                     { key: 'c', route: "<?= route('admin.page.page.create') ?>" }
                 ]
             });
-        });
-        $('#modal-delete-confirmation').on('show.bs.modal', function (event) {
-            var button = $(event.relatedTarget);
-            var actionTarget = button.data('action-target');
-            var modal = $(this);
-            modal.find('form').attr('action', actionTarget);
         });
         $(function () {
             $('.data-table').dataTable({
