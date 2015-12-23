25ec5f9e141aba19c1174ad6fdf9d0dfcbc89b58
Nicolas Widart <n.widart@gmail.com>
2015-12-23 12:05:45 +0100

    Allow to have slugs with multiple parts.

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -4,6 +4,6 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 if (! App::runningInConsole()) {
-    $router->get('{uri}', ['uses' => 'PublicController@uri', 'as' => 'page']);
     $router->get('/', ['uses' => 'PublicController@homepage', 'as' => 'homepage']);
+    $router->any('{uri}', ['uses' => 'PublicController@uri', 'as' => 'page'])->where('uri', '.*');
 }
