8db5eee68e44475dd013cc8c05683dc48007f4ae
Nicolas Widart <n.widart@gmail.com>
2016-05-17 21:17:43 +0200

    Adding the ability to dynamically add middleware on the page module routes

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -4,6 +4,14 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 if (! App::runningInConsole()) {
-    $router->get('/', ['uses' => 'PublicController@homepage', 'as' => 'homepage']);
-    $router->any('{uri}', ['uses' => 'PublicController@uri', 'as' => 'page'])->where('uri', '.*');
+    $router->get('/', [
+        'uses' => 'PublicController@homepage',
+        'as' => 'homepage',
+        'middleware' => config('asgard.page.config.middleware'),
+    ]);
+    $router->any('{uri}', [
+        'uses' => 'PublicController@uri',
+        'as' => 'page',
+        'middleware' => config('asgard.page.config.middleware'),
+    ])->where('uri', '.*');
 }
