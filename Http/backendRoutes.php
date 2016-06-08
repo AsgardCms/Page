2cee915aeaf812eb27149579996902d2c69c54af
Nicolas Widart <n.widart@gmail.com>
2016-06-08 15:22:17 +0200

    Using $router variable

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -7,11 +7,11 @@ $router->bind('page', function ($id) {
     return app(\Modules\Page\Repositories\PageRepository::class)->find($id);
 });
 
-$router->group(['prefix' => '/page'], function () {
-    get('pages', ['as' => 'admin.page.page.index', 'uses' => 'PageController@index']);
-    get('pages/create', ['as' => 'admin.page.page.create', 'uses' => 'PageController@create']);
-    post('pages', ['as' => 'admin.page.page.store', 'uses' => 'PageController@store']);
-    get('pages/{page}/edit', ['as' => 'admin.page.page.edit', 'uses' => 'PageController@edit']);
-    put('pages/{page}/edit', ['as' => 'admin.page.page.update', 'uses' => 'PageController@update']);
-    delete('pages/{page}', ['as' => 'admin.page.page.destroy', 'uses' => 'PageController@destroy']);
+$router->group(['prefix' => '/page'], function (Router $router) {
+    $router->get('pages', ['as' => 'admin.page.page.index', 'uses' => 'PageController@index']);
+    $router->get('pages/create', ['as' => 'admin.page.page.create', 'uses' => 'PageController@create']);
+    $router->post('pages', ['as' => 'admin.page.page.store', 'uses' => 'PageController@store']);
+    $router->get('pages/{page}/edit', ['as' => 'admin.page.page.edit', 'uses' => 'PageController@edit']);
+    $router->put('pages/{page}/edit', ['as' => 'admin.page.page.update', 'uses' => 'PageController@update']);
+    $router->delete('pages/{page}', ['as' => 'admin.page.page.destroy', 'uses' => 'PageController@destroy']);
 });
