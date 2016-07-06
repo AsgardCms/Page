ae933fc5facc9bab28d40acb4f38984f29440564
Nicolas Widart <n.widart@gmail.com>
2016-07-06 21:53:45 +0200

    Adding nicer permission labels

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,9 +2,9 @@
 
 return [
     'page.pages' => [
-        'index',
-        'create',
-        'edit',
-        'destroy',
+        'index' => trans('page::pages.list resource'),
+        'create' => trans('page::pages.create resource'),
+        'edit' => trans('page::pages.edit resource'),
+        'destroy' => trans('page::pages.destroy resource'),
     ],
 ];
